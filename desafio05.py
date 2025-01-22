0c1d6097d3ce87055185b27b7e2b0d510a34bb48
Paulo Henrique <127166560+PaulohenriDev92@users.noreply.github.com>
2025-01-21 22:30:04 -0300

    Add files via upload

---
+++ desafio05.py
@@ -0,0 +1,8 @@
+num = int (input('Digite um número e saiba do seu sucessor e antecessor: '))
+
+suc = num + 1                   # \n  significa nova linha ou pula uma linha
+ant = num - 1
+
+print(f'O sucessor de: {num} é\n {suc}')
+print(f'O antecessor de: {num} é\n {ant}')
+#ALGORÍTMO DE SUCESSOR E ANTECESSOR C/ VARIÁVEL
